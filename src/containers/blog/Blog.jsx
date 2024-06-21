5a0c9db04ff1e4635c5639f4b27cdc827ca642e2
Gourav Wakalwar <gourav.wakalwar123@gmail.com>
2024-06-21 11:53:01 +0530

    importsjs file

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Article }from '../../components';
+import{blog01,blog02,blog03,blog04,blog05 } from './imports'
 import './blog.css'; 
 
 const Blog =() => {
   return (
-      <div>
-          Blog
+      <div className='war1__blog section__padding' id='blog'>
+        <div className='war1__blog-heading'>
+          <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+        </div>
+         <div className="war1__blog-container">
+          <div className="war1__blog-container_groupA">
+           <Article imgUrl={blog01} date='june 21, 2024' title='WAR-1 and OpenAI is the future, Let us explore how it is?'/>
+          </div>
+          <div className="war1__blog-container_groupB">
+           <Article imgUrl={blog02} date='june 21, 2024' title='WAR-1 and OpenAI is the future, Let us explore how it is?'/>
+           <Article imgUrl={blog03} date='june 21, 2024' title='WAR-1 and OpenAI is the future, Let us explore how it is?'/>
+           <Article imgUrl={blog04} date='june 21, 2024' title='WAR-1 and OpenAI is the future, Let us explore how it is?'/>
+           <Article imgUrl={blog05} date='june 21, 2024' title='WAR-1 and OpenAI is the future, Let us explore how it is?'/>
+          </div>
+          </div> 
+
       </div>
   )
 }
